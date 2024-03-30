e7d407db91679be1639630f5d204fdd104664087
cbxpb <2093362891@qq.com>
2024-03-30 18:04:39 +0800

    管理员Service层

--- music/src/main/java/org/xpb/service/AdminService.java
+++ music/src/main/java/org/xpb/service/AdminService.java
@@ -3,6 +3,7 @@ package org.xpb.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.xpb.domain.Admin;
 import org.xpb.exception.ServiceException;
@@ -12,81 +13,69 @@ import org.xpb.util.JwtUtil;
 import javax.annotation.Resource;
 import java.util.List;
 
+
+/**
+ * 管理员Service层
+ */
 @Service
 public class AdminService extends ServiceImpl<AdminMapper,Admin> {
 
     @Resource
     AdminMapper adminMapper;
 
-//    //验证管理员账户是否合法
-//    public Admin login(Admin admin) {
-//        //根据管理员账号查询数据库的管理员信息
-//        Admin dbAdmin = adminMapper.selectByname(admin.getName());
-//        if (dbAdmin == null) {
-//            //抛出一个自定义的异常
-//            throw new ServiceException("用户名或密码错误");
-//        }
-//        if (!admin.getPassword().equals(dbAdmin.getPassword())) {
-//            throw new ServiceException("用户名或密码错误");
-//        }
-//        //生成token
-//        String token = JwtUtil.createToken(dbAdmin.getId().toString());
-//        dbAdmin.setToken(token);
-//        return dbAdmin;
-//    }
-//
+    /**
+     * 根据用户名查询数据库的管理员信息
+     * @param username
+     * @return
+     */
+    public Admin selectByUsername(String username) {
+        QueryWrapper<Admin> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        return  getOne(queryWrapper);
+    }
+
+    /**
+     * 管理员实现登录业务
+     * @param admin
+     * @return
+     */
+    public Admin login(Admin admin) {
+        //1.根据用户名查询数据库的管理员信息，管理员信息为空则抛出一个自定义的异常
+        Admin dbAdmin = selectByUsername(admin.getUsername());
+        if (dbAdmin == null) {
+            throw new ServiceException("用户名或密码错误");
+        }
+        //2.比较密码（MD5、Bcrypt），密码错误则抛出一个自定义的异常
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        boolean matches = passwordEncoder.matches(admin.getPassword(),dbAdmin.getPassword());
+        if (!matches) {
+            throw new ServiceException("用户名或密码错误");
+        }
+        //3.生成token并返回给前端进行身份验证
+        String token = JwtUtil.createToken(dbAdmin.getId().toString(),dbAdmin.getRole());
+        dbAdmin.setToken(token);
+        //4.将密码置空，更安全
+        dbAdmin.setPassword(null);
+        return dbAdmin;
+    }
+
 //    public Admin register(Admin admin) {
 //        //根据管理员输入的账号查询数据库的管理员信息
-//        Admin dbAdmin = adminMapper.selectByname(admin.getName());
+//        Admin dbAdmin = selectByUsername(admin.getUsername());
 //        if (dbAdmin != null) {
 //            //抛出一个自定义的异常
 //            throw new ServiceException("用户名已存在");
 //        }
-//        adminMapper.insert(admin);
+//        save(admin);
 //        return admin;
 //    }
-//
-//    public List<Admin> selectAll() {
-//        return adminMapper.selectAll();
-//    }
 
-
-    public Admin selectByUsername(String username) {
-        QueryWrapper<Admin> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("username",username);
-        //根据用户名查询数据库的管理员信息
-        return  getOne(queryWrapper);
-    }
-
-    //验证管理员账户是否合法
-    public Admin login(Admin admin) {
-        Admin dbAdmin = selectByUsername(admin.getUsername());
-        if (dbAdmin == null) {
-            //抛出一个自定义的异常
-            throw new ServiceException("用户名或密码错误");
-        }
-        if (!admin.getPassword().equals(dbAdmin.getPassword())) {
-            throw new ServiceException("用户名或密码错误");
-        }
-        //生成token
-        String token = JwtUtil.createToken(dbAdmin.getId().toString(),dbAdmin.getRole());
-        dbAdmin.setToken(token);
-        return dbAdmin;
-    }
-
-    public Admin register(Admin admin) {
-        //根据管理员输入的账号查询数据库的管理员信息
-        Admin dbAdmin = selectByUsername(admin.getUsername());
-        if (dbAdmin != null) {
-            //抛出一个自定义的异常
-            throw new ServiceException("用户名已存在");
-        }
-        save(admin);
-        return admin;
-    }
-
+    /**
+     * 管理员实现密码重置业务
+     * @param admin
+     */
     public void resetPassword(Admin admin) {
-        //根据管理员输入的账号查询数据库的管理员信息
+        //1.根据用户名查询数据库的管理员信息，管理员信息为空则抛出一个自定义的异常
         Admin dbAdmin = selectByUsername(admin.getUsername());
         if (dbAdmin == null) {
             //抛出一个自定义的异常
