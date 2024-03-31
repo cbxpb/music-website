c8d79fdae2842d01b785162f464972901b8c5fdc
cbxpb <2093362891@qq.com>
2024-03-31 13:56:21 +0800

    歌手实体类

--- music/src/main/java/org/xpb/domain/Singer.java
+++ music/src/main/java/org/xpb/domain/Singer.java
@@ -16,17 +16,23 @@ import java.util.Date;
 @Data
 @TableName("singer")
 public class Singer {
+    /** 主键id 自动递增 **/
     @TableId(type=IdType.AUTO)
     private Integer id;
+    /** 歌手姓名 **/
     private String name;
+    /** 歌手性别（男，女，组合，不明） **/
     private String gender;
-    private String pic;     //显示图片地址
+    /** 歌手生日 **/
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     @DateTimeFormat(pattern = "YYYY-MM-DD")
     private Date birth;
+    /** 歌手所属地区 **/
     private String location;
+    /** 歌手简介 **/
     private String introduction;
-    private String filename;    //文件名
-    private String url;         //本地存储路径
-
+    /** 歌手图片下载地址 **/
+    private String pic;
+    /** 歌手图片本地地址 **/
+    private String picLocal;
 }
