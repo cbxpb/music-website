e8c024f2f9dc928f7147af144c55e9238e440177
cbxpb <2093362891@qq.com>
2024-03-31 13:59:48 +0800

    管理员Mapper层

--- music/src/main/java/org/xpb/mapper/AdminMapper.java
+++ music/src/main/java/org/xpb/mapper/AdminMapper.java
@@ -1,13 +1,11 @@
 package org.xpb.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 import org.xpb.domain.Admin;
 
 /**
  * 管理员Mapper层
  */
-@Mapper
 public interface AdminMapper extends BaseMapper<Admin> {
 
 }
