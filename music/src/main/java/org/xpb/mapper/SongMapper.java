52b0ef43b986d6b4bdb48a534aedd6206cfae7dd
cbxpb <2093362891@qq.com>
2024-03-31 16:41:33 +0800

    歌曲Mapper层

--- music/src/main/java/org/xpb/mapper/SongMapper.java
+++ music/src/main/java/org/xpb/mapper/SongMapper.java
@@ -1,46 +1,11 @@
 package org.xpb.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Param;
 import org.xpb.domain.Song;
 
-import java.util.List;
-
 /**
- * 歌曲
+ * 歌曲Mapper层
  */
 public interface SongMapper extends BaseMapper<Song> {
 
-
-
-
-//    /**
-//     * 修改
-//     */
-//    public int update(@Param("song") Song song);
-//
-//    /**
-//     * 删除
-//     */
-//    public int delete(Integer id);
-//
-//    /**
-//     * 根据主键查询整个对象
-//     */
-//    public Song selectByID(Integer id);
-//
-//    /**
-//     * 查询所有歌曲
-//     */
-//    public List<Song> allSong();
-//
-//    /**
-//     * 根据歌名模糊查询列表
-//     */
-//    public List<Song> songOfName(String name);
-//
-//    /**
-//     * 根据歌手id查询
-//     */
-//    public List<Song> songOfSingerId(Integer singerId);
 }
