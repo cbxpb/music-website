dd7a96097d55c35c2e29769b55d8d90d1b0c803e
cbxpb <2093362891@qq.com>
2024-03-31 17:53:40 +0800

    歌单Service层

--- music/src/main/java/org/xpb/service/SongListService.java
+++ music/src/main/java/org/xpb/service/SongListService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.xpb.domain.SongList;
 import org.xpb.mapper.SongListMapper;
 
+/**
+ * 歌单Service层
+ */
 @Service
 public class SongListService extends ServiceImpl<SongListMapper, SongList> {
 
