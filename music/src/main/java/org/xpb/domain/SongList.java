901bf5d8d9c8bedb6e23730281a7f8ae4a4f9ac0
cbxpb <2093362891@qq.com>
2024-03-31 17:51:27 +0800

    歌单实体类

--- music/src/main/java/org/xpb/domain/SongList.java
+++ music/src/main/java/org/xpb/domain/SongList.java
@@ -3,11 +3,7 @@ package org.xpb.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 /**
  * 歌单表
@@ -15,12 +11,17 @@ import java.util.Date;
 @Data
 @TableName("song_list")
 public class SongList {
-
+    /** 主键id 自动递增 **/
     @TableId(type = IdType.AUTO)
-    private Integer id;              //主键id
-    private String title;            //歌单标题
-    private String introduction;     //歌单简介
-    private String style;            //歌单风格
-    private String pic;              //图片下载地址
-    private String picLocal;         //图片本地地址
+    private Integer id;
+    /** 歌单标题 **/
+    private String title;
+    /** 歌单简介 **/
+    private String introduction;
+    /** 歌单风格 **/
+    private String style;
+    /** 图片下载地址 **/
+    private String pic;
+    /** 图片本地地址 **/
+    private String picLocal;
 }
