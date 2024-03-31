26f88c1cbaed1dc8648a65b2adff0e9283d5fc7f
cbxpb <2093362891@qq.com>
2024-03-31 18:43:58 +0800

    歌单歌曲Mapper层

--- music/src/main/java/org/xpb/mapper/ListSongMapper.java
+++ music/src/main/java/org/xpb/mapper/ListSongMapper.java
@@ -1,16 +1,12 @@
 package org.xpb.mapper;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.xpb.domain.ListSong;
-import org.xpb.domain.Song;
 
 import java.util.List;
 
 /**
- * 歌曲
+ * 歌单歌曲Mapper层
  */
 public interface ListSongMapper extends BaseMapper<ListSong> {
 
@@ -21,6 +17,4 @@ public interface ListSongMapper extends BaseMapper<ListSong> {
     //2.调用mapper层的方法，查询当前页的数据集合
     List<ListSong> selectByPage(Integer startIndex, Integer pageSize, String name, Integer songListId);
 
-    //mybatisplus分页测试
-    //Page<ListSong> selectPageOne(Page<ListSong> objectPage, QueryWrapper<Song> queryWrapper);
 }
