f52d44726e0e4af1141223a736352c2eb43f5be0
cbxpb <2093362891@qq.com>
2024-03-31 16:40:34 +0800

    歌曲实体类

--- music/src/main/java/org/xpb/domain/Song.java
+++ music/src/main/java/org/xpb/domain/Song.java
@@ -16,22 +16,34 @@ import java.util.Date;
 @TableName("song")
 public class Song {
 
+    /** 主键id自动递增 **/
     @TableId(type = IdType.AUTO)
-    private Integer id;              //主键id
-    private Integer singerId;       //歌手id
-    private String name;            //歌曲名称
-    private String album;           //专辑
+    private Integer id;
+    /** 歌手id **/
+    private Integer singerId;
+    /** 歌曲名称 **/
+    private String name;
+    /** 歌曲专辑 **/
+    private String album;
+    /** 创建时间 **/
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     @DateTimeFormat(pattern = "YYYY-MM-DD")
-    private Date createTime;        //创建时间
+    private Date createTime;
+    /** 修改时间 **/
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     @DateTimeFormat(pattern = "YYYY-MM-DD")
-    private Date updateTime;        //修改时间
-    private String pic;             //图片下载地址
-    private String lyric;           //歌词下载地址
-    private String url;             //歌曲下载地址
-    private String picLocal;       //图片本地地址
-    private String lyricLocal;     //歌词本地地址
-    private String urlLocal;       //歌曲本地地址
+    private Date updateTime;
+    /** 图片下载地址 **/
+    private String pic;
+    /** 歌词下载地址 **/
+    private String lyric;
+    /** 歌曲下载地址 **/
+    private String url;
+    /** 图片本地地址 **/
+    private String picLocal;
+    /** 歌词本地地址 **/
+    private String lyricLocal;
+    /** 歌曲本地地址 **/
+    private String urlLocal;
 
 }
